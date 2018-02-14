5b8e230d7748c7423e746c06a38ddf45528fa081
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-14 19:50:35 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+ 
+    if !@roster.key?(grade)
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+
+    @roster[grade]
+  end
+  
+  def sort
+    sorted = {}
+    sorted_keys = @roster.keys.sort
+    sorted_values = @roster.values
+    
+  end
+end
